31e60fb10eee521da16e02b612da354953f12e8a
Krotton <krotton@gmail.com>
2015-02-01 13:39:56 +0100

    The camera tries to follow location.

--- Wonsz9000/camera.cpp
+++ Wonsz9000/camera.cpp
@@ -26,13 +26,22 @@ void Camera::apply() const
 		// of the camera by translating the target location by the specified
 		// distance vector.
 		target = target_entity_->location();
-		cam = target + dist_;
+		auto dist = dist_;
 
 		// Following cameras must also follow rotation!
-		/*auto const rot = target_entity_->rotation();
-		glRotated(rot.x, 1.0, 0.0, 0.0);
-		glRotated(rot.y, 0.0, 1.0, 0.0);
-		glRotated(rot.z, 0.0, 0.0, 1.0);*/
+		// Notice: only rotation around Y!
+		// Rodrigues formula: v_rot = vcos(a) + (k x v)sin(a) + k(k * v)(1 - cos(a))
+		auto const rot = target_entity_->rotation();
+		double const ca = cos(rot.y * 3.141592 / 180.0), sa = sin(rot.y * 3.141592 / 180.0);
+		double const cross_x = -dist.y * 0.0 + dist.z * 1.0,
+			cross_y = -dist.z * 0.0 + dist.x * 0.0,
+			cross_z = -dist.x * 1.0 + dist.y * 0.0;
+		double const dot = dist.x * 0.0 + dist.y * 1.0 + dist.z * 0.0;
+		double const rx = dist.x * ca + cross_x * sa + dot * 0.0 * (1 - ca),
+			ry = dist.y * ca + cross_y * sa + dot * 1.0 * (1 - ca),
+			rz = dist.z * ca + cross_z * sa + dot * 0.0 * (1 - ca);
+
+		cam = target + Vec3D{rx, ry, rz};
 	}
 
 	gluLookAt(cam.x, cam.y, cam.z,
