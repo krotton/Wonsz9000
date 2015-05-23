76d310a5f6f08d4cb2c6bde005138dfa0966674f
Krotton <krotton@gmail.com>
2015-05-23 20:22:06 +0200

    Added Xcode project, toolchain-specific .gitignore.

---
+++ Wonsz9000/Wonsz9000/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Wonsz9000
+//
+//  Created by Krotton on 23.05.2015.
+//  Copyright (c) 2015 krotton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
