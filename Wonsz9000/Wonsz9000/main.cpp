57f10da56d3efb6583dfdeb3bd1d5e0bd4a02274
Krotton <krotton@gmail.com>
2015-05-26 00:01:20 +0200

    Created a window and OpenGL context.

--- Wonsz9000/Wonsz9000/main.cpp
+++ Wonsz9000/Wonsz9000/main.cpp
@@ -8,5 +8,30 @@ int main(int argc, const char * argv[])
         return 1;
     }
     
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLFWwindow *window(glfwCreateWindow(1024, 768, "Wonsz9000", nullptr, nullptr));
+    if (!window)
+    {
+        std::cout << "[FATAL] Could not create a window!" << std::endl;
+        glfwTerminate();
+        return 2;
+    }
+    
+    glfwMakeContextCurrent(window);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    
+    do {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    } while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+             !glfwWindowShouldClose(window));
+    
+    glfwDestroyWindow(window);
+    
     return 0;
 }
